8217d85a39a87b455895355506b7a2b039508ff7
TheMasterGeese <themastergeese@gmail.com>
2022-04-22 12:10:31 -0400

    build overhaul

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require("gulp");
-const zip = require('gulp-zip');
-
-gulp.task("zip", function () {
-    return gulp.src(['lang*/**/*', 'dist/**/*', 'module.json'])
-        .pipe(zip('ready-check.zip'))
-        .pipe(gulp.dest('out'));
-});
-
